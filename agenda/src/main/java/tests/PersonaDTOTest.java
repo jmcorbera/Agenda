e2fd43ca361594443cef91ed5c9141b287335e7f
solpierozzi <solpierozzi@hotmail.com>
2020-09-22 17:50:24 -0300

    Agregado test isValid PersonaDTO

---
+++ agenda/src/main/java/tests/PersonaDTOTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import dto.PersonaDTO;
+
+public class PersonaDTOTest {
+	@Test
+	public void personaValidaTest() {
+		PersonaDTO persona = new PersonaDTO(0, "Nombre", "1142203912","2020/01/01","nombreMail@hotmail.com","");
+		assertTrue(persona.isValid().isEmpty());
+	}
+	
+	@Test
+	public void personaInvalidaTestEmailInvalidoFaltaArroba() {
+		PersonaDTO persona = new PersonaDTO(0, "Nombre", "1142203912","2020/01/01","nombreMailhotmail.com","");
+		assertTrue(!persona.isValid().isEmpty());
+	}
+	
+	@Test
+	public void personaInvalidaTestEmailInvalidoFaltaPunto() {
+		PersonaDTO persona = new PersonaDTO(0, "Nombre", "1142203912","2020/01/01","nombreMail@hotmailcom","");
+		assertTrue(!persona.isValid().isEmpty());
+	}
+	
+	@Test
+	public void personaInvalidaTestEmailInvalidoFaltaLetrasDespuesPunto() {
+		PersonaDTO persona = new PersonaDTO(0, "Nombre", "1142203912","2020/01/01","nombreMailhotmail.","");
+		assertTrue(!persona.isValid().isEmpty());
+	}
+	
+	@Test
+	public void personaInvalidaTestFaltaFormaContacto() {
+		PersonaDTO persona = new PersonaDTO(0, "Nombre", "","2020/01/01","","");
+		assertTrue(!persona.isValid().isEmpty());
+	}
+	
+	@Test
+	public void personaInvalidaTestFaltaNombre() {
+		PersonaDTO persona = new PersonaDTO(0, "", "1142203912","2020/01/01","nombreMail@hotmail.com","");
+		assertTrue(!persona.isValid().isEmpty());
+	}
+}
